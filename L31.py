a958a515b05b291263a493a596b72416e7f6a0bf
Breanna <123026437+breyvonne@users.noreply.github.com>
2023-02-08 16:14:36 -0500

    Add files via upload

---
+++ L31.py
@@ -0,0 +1,15 @@
+#Breanna Eafon
+import turtle
+fred=turtle.Turtle()
+fred.color("purple")
+fred.forward(100)
+fred.right(35)
+fred.forward(140)
+fred.right(90)
+fred.forward(100)
+#After we ran the code an image came on the screen in the color that we chose and moved accross the screen
+#In the first line you import the function
+#fred.color assigns a color to fred
+#fred.forward makes the turtle move to the right
+#fred.right makes the turtle turn by x degrees
+#changing the fred.right argument to 90 made the turtle turn at a right angle
---
+++ L35.py
@@ -0,0 +1,11 @@
+#Breanna Eafon
+
+def song():
+    print("I have a dream")
+
+def songrepeat():
+    song()
+    song()
+    song()
+
+songrepeat()
